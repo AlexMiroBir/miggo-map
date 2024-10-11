8f5aaf8f90e8004a684581c2b2e394018d691492
Alekseim <Alekseim@anyclip-dm.com>
2024-10-11 21:47:20 +0300

    client: - fixed tracking line (only track, without arrows, counting arrow's angle is very expensive)

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {MapContainer, Marker, TileLayer, Polyline, useMap, useMapEvents} from 'react-leaflet'
+import {MapContainer, Marker, Polyline, TileLayer, useMap, useMapEvents} from 'react-leaflet'
 import styles from './Map.module.css'
 import L from "leaflet";
 
@@ -38,17 +38,10 @@ const Map = ({coordinates}) => {
         localStorage.setItem('mapZoom', JSON.stringify(zoom));
     }, [zoom]);
 
-    const stopIcon = new L.Icon({
-        iconUrl: 'https://www.pngall.com/wp-content/uploads/2016/07/Arrow-Free-Download-PNG.png',
-        iconSize: [10, 10],
-        iconAnchor: [0, 5],
-        popupAnchor: [0, 0],
-    });
-
     const spaceShipIcon = new L.Icon({
         iconUrl: 'https://cdn-icons-png.flaticon.com/512/1945/1945842.png',
         iconSize: [50, 50],
-        iconAnchor: [20, 20],
+        iconAnchor: [25, 25],
         popupAnchor: [0, 0],
     });
 
@@ -61,10 +54,7 @@ const Map = ({coordinates}) => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {coordinates.map((position, index) =>
-                    <Marker key={index} position={position}
-                            icon={index === coordinates.length - 1 ? spaceShipIcon : stopIcon}/>
-                )}
+                <Marker position={coordinates.at(-1)} icon={spaceShipIcon}/>
                 <Polyline positions={coordinates} color="red"/>
                 <MapUpdater coordinates={coordinates} zoom={zoom}/>
                 <ZoomListener setZoom={setZoom}/>
