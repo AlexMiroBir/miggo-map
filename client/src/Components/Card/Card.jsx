c34beebbdfc2bf7647163601fba0a2c153d4012a
Alekseim <Alekseim@anyclip-dm.com>
2024-10-11 21:56:54 +0300

    client: - fixed info Card (datetime format)

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -5,12 +5,12 @@ const Card = ({locations}) => {
     const location = locations.at(-1) ?? {};
     return (
         <div className={styles.Container}>
-            <div>Latitude: {location.latitude}</div>
-            <div>Longitude: {location.longitude}</div>
-            <div>Height: {location.height} km</div>
-            <div>Velocity: {location.velocity} km/h</div>
-            <div>Period: {location.period} min</div>
-            <div>Updated: {location.timestamp?.toLocaleString()}</div>
+            <div>Latitude: <b>{location.latitude}</b></div>
+            <div>Longitude: <b>{location.longitude}</b></div>
+            <div>Height:<b> {location.height} km</b></div>
+            <div>Velocity: <b>{location.velocity} km/h</b></div>
+            <div>Period: <b>{location.period} min</b></div>
+            <div>Updated: <b>{new Date(location.timestamp)?.toLocaleString('he-IL')}</b></div>
         </div>
     );
 };
